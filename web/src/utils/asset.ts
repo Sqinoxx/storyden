cdc48176198656976f78f5996069d7a406bc9fb6
Sqinoxx <schmitzy008@gmail.com>
2026-07-24 15:55:28 +0200

    download works

--- web/src/utils/asset.ts
+++ web/src/utils/asset.ts
@@ -1,7 +1,11 @@
 import { API_ADDRESS } from "@/config";
 
 export function getAssetURL(filename?: string) {
-  if (!filename) return;
-
-  return `${API_ADDRESS}${filename}`;
+  if (!filename) return "";
+  if (filename.startsWith("http://") || filename.startsWith("https://")) {
+    return filename;
+  }
+  const prefix = filename.startsWith("/") ? "" : "/";
+  return `${API_ADDRESS}${prefix}${filename}`;
 }
+
